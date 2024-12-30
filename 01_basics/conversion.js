ca4ce27c6d660b58e0e79e85e010e41fed47785a
EKTA MAURYA <mauryae440@gmail.com>
2024-12-30 15:38:28 +0000

    conversion of datatype

---
+++ 01_basics/conversion.js
@@ -0,0 +1,27 @@
+let score = 33
+console.log(typeof score);
+console.log(typeof(score));
+let name = "Ekta"
+console.log(name);
+let num = true
+console.log(num)
+
+let isLoggedin = 1
+let boll = Boolean(isLoggedin)
+console.log(boll)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ 01_basics/variable.js
@@ -0,0 +1,5 @@
+const accountId = 123412
+let accountEmail = "ekta@gmail.com"
+var accountpassword = "!23678"
+accountCity = "Kanpur"
+console.table([accountId,accountEmail,accountpassword,accountCity]);
