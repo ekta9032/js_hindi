c5a1d8e2537b769870e3c36cc750f6f54c4eb15d
EKTA MAURYA <mauryae440@gmail.com>
2024-12-31 19:25:32 +0000

    datatype

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,13 @@
+//primitive data type
+//7 Types: string, number, boolean, null, undefined, symbol,BigInt
+
+const score = 100
+const scoreValue = 100.45
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('1223')
+console.table([score,scoreValue,isLoggedIn,outsideTemp,userEmail,id,anotherId])
+// Reference (Non primitive)
+//Array,Objects, functions
